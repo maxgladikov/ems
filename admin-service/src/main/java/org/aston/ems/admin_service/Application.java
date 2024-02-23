1d0a8eaae0aef1c6c13d7e02d0fa0dccf7e4b1e9
max <msgladikov@gmail.com>
2024-02-23 12:45:50 +0500

    common-library was added

--- admin-service/src/main/java/org/aston/ems/admin_service/Application.java
+++ admin-service/src/main/java/org/aston/ems/admin_service/Application.java
@@ -1,5 +1,6 @@
 package org.aston.ems.admin_service;
 
+import org.aston.ems.common_lib.exception.web.EmsResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		new EmsResponse<String>();
 	}
 }
---
+++ common-lib/src/main/java/org/aston/ems/common_lib/exception/web/EmsResponse.java
@@ -0,0 +1,6 @@
+package org.aston.ems.common_lib.exception.web;
+
+public class EmsResponse<T> {
+	private int code;
+	private String message;
+}
