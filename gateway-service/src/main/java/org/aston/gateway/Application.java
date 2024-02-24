4b8e3c1dff5c7092fece4818ad19afeb7cfd2093
max <msgladikov@gmail.com>
2024-02-24 20:17:52 +0500

    finaly it works

--- gateway-service/src/main/java/org/aston/gateway/Application.java
+++ gateway-service/src/main/java/org/aston/gateway/Application.java
@@ -2,24 +2,12 @@ package org.aston.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+			SpringApplication.run(Application.class, args);
 		}
 	
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-			.route("limit_route", r -> r
-				.path("/admin/")
-//				.filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
-				.uri("http://admin-service:9000/api/v1/"))
-			.build();
-	}
 	
 }
