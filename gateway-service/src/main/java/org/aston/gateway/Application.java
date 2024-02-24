8d827d7351ca8ef1fd6a9ece96c7fdd2c2a888b9
max <msgladikov@gmail.com>
2024-02-24 17:12:31 +0500

    first feature

---
+++ gateway-service/src/main/java/org/aston/gateway/Application.java
@@ -0,0 +1,25 @@
+package org.aston.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		}
+	
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route("limit_route", r -> r
+				.host("localhost:8000").and().path("/**")
+//				.filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
+				.uri("http://admin-service:9001/api/v1/"))
+			.build();
+	}
+	
+}
