34adb79a8645de023baba55bdfb570fce70b3c2b
dariakoval <dasharibkavmore666@mail.ru>
2024-03-04 22:59:49 +0400

    create dtos for student-service

---
+++ student-service/src/main/java/org/aston/ems/student_service/dto/TaskUpdateDTO.java
@@ -0,0 +1,22 @@
+package org.aston.ems.student_service.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.aston.ems.student_service.model.Score;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+@Setter
+@Getter
+public class TaskUpdateDTO {
+
+    @NotNull
+    private Long id;
+
+    @NotNull
+    private Long assigneeId;
+
+    private JsonNullable<String> answer;
+
+    private JsonNullable<Score> mark;
+}
