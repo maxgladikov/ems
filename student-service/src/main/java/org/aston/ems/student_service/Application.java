491823a652832e7efbd98cfca1e5d928a77bdded
dariakoval <dasharibkavmore666@mail.ru>
2024-03-04 22:55:59 +0400

    update Application.java

--- student-service/src/main/java/org/aston/ems/student_service/Application.java
+++ student-service/src/main/java/org/aston/ems/student_service/Application.java
@@ -1,9 +1,25 @@
 package org.aston.ems.student_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Student Service",
+				version = "1.0",
+				description = "Student management system",
+				contact = @Contact(
+						name = "Darya Koval",
+						url = "https://github.com/dariakoval"
+				)
+		)
+)
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
