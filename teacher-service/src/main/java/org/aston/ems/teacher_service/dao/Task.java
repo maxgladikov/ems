f3c75af9d3287019aed3c834e04aa23f7e3b146f
Alina Dubrovina <dubrovinaa389@gmail.com>
2024-03-05 09:41:22 +0100

    refactored: working entity

--- teacher-service/src/main/java/org/aston/ems/teacher_service/dao/Task.java
+++ teacher-service/src/main/java/org/aston/ems/teacher_service/dao/Task.java
@@ -1,16 +1,18 @@
 package org.aston.ems.teacher_service.dao;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "task", schema = "app")
 public class Task extends BaseEntity {
     private long studentId;
     private long teacherId;
