43e8aa4fe0d7e8edf3d1132506467ee4937a9679
Alina Dubrovina <dubrovinaa389@gmail.com>
2024-03-11 10:49:08 +0100

    chore: add swagger.yaml

--- teacher-service/src/main/java/org/aston/ems/teacher_service/Application.java
+++ teacher-service/src/main/java/org/aston/ems/teacher_service/Application.java
@@ -2,7 +2,6 @@ package org.aston.ems.teacher_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class Application {
